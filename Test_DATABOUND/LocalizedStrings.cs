2b5610b2cd3605bd71a4d1c08301a0e239fe718a
unknown <srikanth.veda18@gmail.com>
2015-06-03 16:07:53 +0100

    Initial commit

---
+++ Test_DATABOUND/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Test_DATABOUND.Resources;
+
+namespace Test_DATABOUND
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Test_DATABOUND/ViewModels/ItemViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+
+namespace Test_DATABOUND.ViewModels
+{
+    public class ItemViewModel : INotifyPropertyChanged
+    {
+        private string _id;
+        /// <summary>
+        /// Sample ViewModel property; this property is used to identify the object.
+        /// </summary>
+        /// <returns></returns>
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        private string _category;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        /*public string category { get; set; }
+        public string url { get; set; }
+        */
+        public string category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                if (value != _category)
+                {
+                    _category = value;
+                    NotifyPropertyChanged("category");
+                }
+            }
+        }
+
+        private string _url;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                if (value != _url)
+                {
+                    _url = value;
+                    NotifyPropertyChanged("Url");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
